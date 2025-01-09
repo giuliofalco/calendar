59c358041d5cacadb024ca3336523dd4bf51c0a9
Giulio Falco <gfalco58@gmail.com>
2025-01-10 00:44:09 +0100

    url /agenda

--- cal/urls.py
+++ cal/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.calendar_view, name='calendar_view'),
+    path('/agenda', views.calendar_view, name='calendar_view'),
     path('edit/<int:year>/<int:month>/<int:day>/', views.day_editor, name='day_editor'),
 ]
